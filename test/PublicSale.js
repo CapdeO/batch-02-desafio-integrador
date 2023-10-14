83e643ba8dbea86dea7641891b0a657a031158da
Capde <capde22@gmail.com>
2023-10-14 18:13:00 -0300

    liq added

--- test/PublicSale.js
+++ test/PublicSale.js
@@ -8,6 +8,8 @@ const factoryArtifact = require('@uniswap/v2-core/build/UniswapV2Factory.json');
 const routerArtifact = require('@uniswap/v2-periphery/build/UniswapV2Router02.json');
 const pairArtifact = require('@uniswap/v2-periphery/build/IUniswapV2Pair.json');
 
+const WETH9 = require('../WETH9.json');
+
 const MINTER_ROLE = getRole("MINTER_ROLE");
 const BURNER_ROLE = getRole("BURNER_ROLE");
 
@@ -22,45 +24,59 @@ describe("Testeando PublicSale", () => {
         var Factory = new ethers.ContractFactory(factoryArtifact.abi, factoryArtifact.bytecode, owner);
         var factory = await Factory.deploy(owner.address);
 
-        var USDC = await ethers.getContractFactory("USDCoin");
-        var usdc = await USDC.deploy();
-
         var BBTKN = await ethers.getContractFactory("BBitesToken");
         var bbtkn = await upgrades.deployProxy(BBTKN, [], {kind: "uups"});
 
+        var USDC = await ethers.getContractFactory("USDCoin");
+        var usdc = await USDC.deploy();
+
         var createPair = await factory.createPair(bbtkn.target, usdc.target);
-        await createPair.wait();
 
         var pairAddress = await factory.getPair(bbtkn.target, usdc.target);
 
-        //var reserves = await pairAddress.getReserves();
+        var pair = new ethers.Contract(pairAddress, pairArtifact.abi, owner);
+        var reserves;
 
-        var wethAddress = '0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419';
+        var Weth = new ethers.ContractFactory(WETH9.abi, WETH9.bytecode, owner);
+        var weth = await Weth.deploy();
 
         var Router = new ethers.ContractFactory(routerArtifact.abi, routerArtifact.bytecode, owner);
-        var router = await Router.deploy(factory.target, wethAddress);
+        var router = await Router.deploy(factory.target, weth.target);
 
+        await bbtkn.approve(router, bbtkn.balanceOf(owner));
+        await usdc.approve(router, usdc.balanceOf(owner));
 
+        var deadline = Math.floor(Date.now()/1000 + (10*60));
+
+        await router.addLiquidity(
+            bbtkn.target,
+            usdc.target,
+            bbtkn.balanceOf(owner),
+            usdc.balanceOf(owner),
+            0,
+            0,
+            owner,
+            deadline
+        )
+        reserves = await pair.getReserves();
+        //console.log('Reservas del Pool: ', reserves);
         
         // var ContractPublicSale    = await ethers.getContractFactory("PublicSale");
 
         // var contract = await upgrades.deployProxy(ContractPublicSale, ["CuyCollectionNft", "CUY"], { initializer: 'initialize', kind: 'uups' });
         
-        return { factory, usdc, bbtkn, pairAddress, router, owner, alice, bob, carl };
-
-
+        return { factory, bbtkn, usdc, router, owner, alice, bob, carl };
     }
 
     describe("Publicación", () => {
-        it("Name y Symbol", async () => {
+        it("Despliegues y Pool", async () => {
 
-            var { factory, usdc, bbtkn, pairAddress, router, owner, alice } = await loadFixture(loadTest);
+            var { factory, usdc, bbtkn, router, owner, alice } = await loadFixture(loadTest);
 
-            console.log('Factory: ', factory.target);
-            console.log('USDC: ', usdc.target);
-            console.log('BBTKN: ', bbtkn.target);
-            console.log('PairAddress: ', pairAddress);
-            console.log('RouterAddress: ', router.target);
+            //console.log('Factory: ', factory.target);
+            //console.log('USDC: ', usdc.target);
+            //console.log('BBTKN: ', bbtkn.target);
+            //console.log('RouterAddress: ', router.target);
             
 
 
